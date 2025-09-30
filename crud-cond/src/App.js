f491820de7c279d0a6d8e6457a2f3e09a483885b
joaoVictor-Soares <jvtrgmlcmsoares@gmail.com>
2025-09-29 22:39:52 -0300

    NEW: criação da lógica e do estilo

---
+++ crud-cond/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import './App.css'
+
+import Login from './components/Login/Login';
+
+function App(){
+
+const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+const handleLogin = (userName, password) => {
+  if(userName === 'admin' && password === '123'){
+    setIsLoggedIn(true)
+  }
+  else{
+    alert("Usuário ou Senha inválidos")
+  }
+}
+
+if(!isLoggedIn){
+  return <Login onLogin={handleLogin}/>
+}
+
+}export default App;
